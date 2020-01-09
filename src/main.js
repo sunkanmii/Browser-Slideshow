d7835ea6e0499ceb7ec61179e24c8bb2380cc31e
Fafowora Sunkanmi <sunkanmifafowora@gmail.com>
2020-01-09 20:03:34 +0100

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 "use strict";
 if('serviceWorker' in navigator){
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js')
+        navigator.serviceWorker.register('/sw.js')
         .then(reg => {
             console.log('Registrated: ', reg);  
         })
